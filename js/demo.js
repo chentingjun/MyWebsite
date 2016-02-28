f5fe11b15bdc3978f38d999b03db0ca44177064e
chentingjun <1249725368@qq.com>
2016-02-28 13:19:44 +0800

    完善轮转

--- js/demo.js
+++ js/demo.js
@@ -1,10 +1,12 @@
 $(function(){
+	var gotruepicker = "";/*定时器*/
+	var speedpicker = 500;/*定时器的起始速度*/
+	console.log("----内部的speedpicker="+speedpicker);
 	init();/*初始化信息*/
 
 
 	function init(){
 		console.log("%cdemo初始化","color:red;");
-		var gotruepicker = "";/*定时器*/
 		$(window).scroll(function(event) {
 			var nowtop = $(window).scrollTop();
 			var windowheight = $(window).height();
@@ -19,22 +21,44 @@ $(function(){
 			var img = $("#jiugong .jiugongbtn img");
 			if(img.attr("src").indexOf("kaishi") > -1){
 				goTurn(true);
+				console.log(speedpicker);
 				img.attr("src","../img/iconfont-zanting.png");
 			}else{
 				goTurn(false);
+				console.log(speedpicker);
 				img.attr("src","../img/iconfont-kaishi.png");
 			}
 		})
+		pictruelist();
 	}
+
+	/*九宫格转盘 #jiugong*/
 	function goTurn(flag){
 		if(flag){
+			speedpicker = Math.abs(speedpicker - 50 || 100);
 			gotruepicker = setInterval(function(){
 				var num = $("#jiugong .mengceng").data("num");
 				$("#jiugong .mengceng").removeClass('mengceng');
 				$("#jiugong li[data-num="+getturnarr(num)+"]").addClass('mengceng');
-			},50);
+				$("#jiugong .result img").attr("src",$("#jiugong .mengceng img").attr("src"));
+				clearInterval(gotruepicker);
+				goTurn(true);
+			},speedpicker);
 		}else{
-			clearInterval(gotruepicker);
+			speedpicker = Math.abs(speedpicker + 50 || 100);
+			if(speedpicker > 550){
+				clearInterval(gotruepicker);
+			}else{
+				clearInterval(gotruepicker);
+				gotruepicker = setInterval(function(){
+					var num = $("#jiugong .mengceng").data("num");
+					$("#jiugong .mengceng").removeClass('mengceng');
+					$("#jiugong li[data-num="+getturnarr(num)+"]").addClass('mengceng');
+					$("#jiugong .result img").attr("src",$("#jiugong .mengceng img").attr("src"));
+					clearInterval(gotruepicker);
+					goTurn(false);
+				},speedpicker);
+			}
 		}
 	}
 	function getturnarr(num){
@@ -44,6 +68,30 @@ $(function(){
 			return arr[0];
 		}else{
 			return arr[index+1];
+		}
+	}
+	/*图片轮转列表*/
+	function pictruelist(){
+		/*轮转图片列表初始化*/
+		var imgbox = document.getElementById("imgbox");
+		var imglist = imgbox.getElementsByTagName("ul")[0]; 
+		var picnum = imglist.getElementsByTagName("li").length;
+		console.log(picnum);
+		imglist.innerHTML = imglist.innerHTML + imglist.innerHTML;
+		var getRoll = function(){
+			var imglistlastpic = imglist.getElementsByTagName("li")[picnum];
+			if(imgbox.scrollLeft == imglistlastpic.offsetLeft){
+				imgbox.scrollLeft = 0;
+			}else{
+				imgbox.scrollLeft++;
+			}
+		}
+		var timepicker = setInterval(getRoll,20);
+		imgbox.onmouseover = function(){
+			clearInterval(timepicker);
+		}
+		imgbox.onmouseout = function(){
+			timepicker = setInterval(getRoll,20);
 		}
 	}
 })
